0f3357ff5f2b44da12e41df0094b6f133b18feab
raghuaa <36770875+raghuaa@users.noreply.github.com>
2020-08-26 22:38:31 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("***************** HelloWorld Editing in GitHub ****************");
+		System.out.println("***************** Adding Github Webhook instead of SCM Poll ****************");
 	}
 }
